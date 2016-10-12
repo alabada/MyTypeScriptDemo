708d0d7ff356209f824b3de9811d955d64406bd2
zhida.wen <zhida.wen@qishon.com>
2016-10-12 13:17:48 +0800

    modified

---
+++ src/namespace.ts
@@ -0,0 +1,27 @@
+interface StringValidator {
+    isAcceptable(s: string): boolean;
+}
+
+let lettersRegexp = /^[A-Za-z]+$/;
+let numberRegexp = /^[0-9]+$/;
+
+class LettersOnlyValidator implements StringValidator {
+    isAcceptable(s: string) {
+        return lettersRegexp.test(s);
+    }
+}
+
+class ZipCodeValidator implements StringValidator {
+    isAcceptable(s: string) {
+        return s.length === 5 && numberRegexp.test(s);
+    }
+}
+
+
+let string = ["Hello", "99999", "121"];
+
+
+let validators: { [s: string]: StringValidator } = {};
+validators["ZIP code"] = new ZipCodeValidator();
+validators["Letters only"] = new LettersOnlyValidator();
+
